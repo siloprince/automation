d4548fb6364908f7889521cc638d356d388097c4
Yoshiaki Araki <info@tessellation.jp>
2017-04-01 12:59:53 +0900

    polygon

--- viscuitta/viscuitta.js
+++ viscuitta/viscuitta.js
@@ -2,44 +2,80 @@
 'use strict';
 (function(document,window,console){
     let config = {
-        id: 'test'
-        , stage: {
-            width: 1000
-            , height: 1000
-        }
-        , iteration: {
-            delta: 2000
-        }
+        "id": "xxx",
+        "stage": {
+            "width": 1000,
+            "height":1000
+        },
+        "iteration": {
+            "delta": 1000
+        },
+        "polygons": [
+            "<polygon class='square' points='0 0, 30 0, 30 30, 0 30' fill='#aaaaaa' stroke='#0000ff'/>"
+        ],
+        "rules": [
+            { 
+                "pattern":[
+                    ".square"
+                ],
+                "next":[
+                    {
+                        "polygon": 0                 
+                    },
+                    {
+                        "polygon": 0,
+                        "dx": 30,
+                        "dy": 0                   
+                    },
+                    {
+                        "polygon": 0,
+                        "dx": 0,
+                        "dy": 30                       
+                    }
+                ]  
+            }
+        ]
     };
     init();
-    function rectSVG(x, y, args) {
+    function polygonSVG(polygon,x, y, args) {
         let stage = args.stage;
-        stage.insertAdjacentHTML('beforeend',
-            `<rect class="square" x="${x}" y="${y}" width="30" height="30" fill="#ffaaaa" stroke="#0000ff">`
-        );
-    }
-    function rule(args) {
-        let squares = getSquares({ noDup: true });
-        for (let si = 0; si < squares.length; si++) {
-            let square = squares[si];
-            let x0 = square.x + square.width;
-            let y0 = square.y;
-            rectSVG(x0, y0, args);
-            let x1 = square.x;
-            let y1 = square.y + square.height;
-            rectSVG(x1, y1, args);
+        let polygonStr = config.polygons[polygon];
+        stage.insertAdjacentHTML('beforeend',`<g transform="translate(${x},${y})">${polygonStr}</g>`);
+    }    
+    function rules(args) {
+        for (let ri=0;ri<config.rules.length;ri++) {
+            let rule = config.rules[ri];
+            let pattern = rule.pattern;
+            console.dir(rule);
+            console.log(pattern)
+            for (let pi=0;pi<pattern.length;pi++) {
+                let shapes = getPolygons(pattern[pi],{ noDup: true });
+                for (let si = 0; si < shapes.length; si++) {
+                    let shape = shapes[si];
+                    let nexts = rule.next;
+                    for (let ni=0;ni<nexts.length;ni++) {
+                        console.log(ni);
+                        let next = nexts[ni];
+                        let x = 0;
+                        if ('dx' in next) {
+                            x = shape.x + next.dx;
+                        }
+                        let y = 0;
+                        if ('dy' in next) {
+                            y = shape.y + next.dy;
+                        }
+                        polygonSVG(next.polygon, x, y, args);
+                    }
+                }
+            }
         }
     }
-
     function init() {
         updateConfig();
         document.addEventListener('DOMContentLoaded',
             function () {
                 document.body.insertAdjacentHTML('beforeend', '<table><tr><td><svg></svg></td></tr></table>');
                 let svg = document.querySelector('svg');
-        
-                
-
                 svg.setAttribute('width', config.stage.width);
                 svg.setAttribute('height', config.stage.height);
                 svg.innerHTML = '<g id="stage"></g>';
@@ -48,7 +84,8 @@
                     delta: config.iteration.delta
                     , stage: document.querySelector('#stage')
                 };
-                rectSVG(0, 0, args);
+                polygonSVG(0,0,0,args);
+                //rectSVG(0, 0, args);
                 main(count, args);
             }, false);
     }
@@ -82,35 +119,35 @@
     }
     function main(count, args) {
         console.log(count);
-        rule(args);
+        rules(args);
         let nextMain = (function (c, a) {
             return function () { main(c + 1, a);};
         })(count, args);
         window.setTimeout(nextMain, args.delta);
     }
-
-    function getSquares(opt) {
+    function getPolygons(match,opt) {
         let ret = [];
         let dup = {};
-        let squares = document.querySelectorAll('rect.square');
+        let squares = document.querySelectorAll(`polygon${match}`);
         for (let si = 0; si < squares.length; si++) {
             let square = squares[si];
-            let x = parseInt(square.getAttribute('x'), 10);
-            let y = parseInt(square.getAttribute('y'), 10);
-            let width = parseInt(square.getAttribute('width'), 10);
-            let height = parseInt(square.getAttribute('height'), 10);
-            let uniq = `${x}:${y}:${width}:${height}`;
+            let transform = square.parentNode.getAttribute('transform').trim();
+            let x = 0;
+            let y = 0;
+            if (/translate\(\s*(-*[0-9]*\.*[0-9]+)\s*,(\s*(-*[0-9]*\.*[0-9]+)\s*)\)/.test(transform)){
+                x = RegExp.$1;
+                y = RegExp.$2;
+                x = parseInt(x,10);
+                y = parseInt(y,10);
+            }
+            let uniq = `${x}:${y}`;
             if (('noDup' in opt) && uniq in dup) {
                 continue;
             }
             dup[uniq] = true;
             ret.push({
-                id: square.id
-                , x: x
+                x: x
                 , y: y
-                , width: width
-                , height: height
-                , uniq: uniq
             });
         }
         return ret;
