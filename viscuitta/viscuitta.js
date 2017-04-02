379ec35f371399139e002881b2312180b66a7a9d
Yoshiaki Araki <info@tessellation.jp>
2017-04-02 11:36:26 +0900

    terminate

--- viscuitta/viscuitta.js
+++ viscuitta/viscuitta.js
@@ -2,53 +2,71 @@
 'use strict';
 (function(document,window,console){
     let config = {
-        "id": "xxx",
-        "stage": {
-            "width": 300,
-            "height":300
+        'id': 'xxx',
+        'stage': {
+            'width': 300,
+            'height':300
         },
-        "iteration": {
-            "dt": 1000,
-            "scaleLimit": 0.1,
-            "stepLimit": 10000,
-            "shapeCount": true
+        'iteration': {
+            'dt': 1000,
+            'scaleLimit': 0.1,
+            'stepLimit': 10000,
+            'shapeCount': true
         },
-        "polygons": [
-            "<polygon class='square' points='0 0, 30 0, 30 30, 0 30' fill='#aaaaaa' stroke='#0000ff'/>"
+        'polygons': [
+            '<polygon class=\'square\' points=\'0 0, 30 0, 30 30, 0 30\' fill=\'#aaaaaa\' stroke=\'#0000ff\'/>'
         ],
-        "rules": [
+        'rules': [
             { 
-                "pattern":[
-                    ".square"
+                'pattern':[
+                    '.square'
                 ],
-                "next":[
+                'next':[
                     {
-                        "polygon": 0                 
+                        'polygon': 0,
+                        'terminate': 1                 
                     },
                     {
-                        "polygon": 0,
-                        "dx": 30,
-                        "dy": 0                   
+                        'polygon': 0,
+                        'dx': 30,
+                        'dy': 0                   
                     },
                     {
-                        "polygon": 0,
-                        "dx": 0,
-                        "dy": 30                       
+                        'polygon': 0,
+                        'dx': 0,
+                        'dy': 30                       
                     }
                 ]  
             }
         ]
     };
+    let terminateHash = {};
+    let stageHash = {};
     init();
-    function polygonSVG(polygon,x, y, r, s, args) {
-        let stage = args.stage;
+    function polygonSVG(polygon,x, y, r, s, t, args) {
+        let key = `${polygon}:${x}:${y}:${r}:${s}`;
+        if (key in terminateHash) {
+            return;
+        }
+        if (key in stageHash) {
+            return;
+        }        
+        let g;     
+        if (t) {
+            g = args.terminate;
+            terminateHash[key]=true;
+        } else {
+            g = args.stage;
+            stageHash[key]=true;
+        }
         let polygonStr = config.polygons[polygon];
-        stage.insertAdjacentHTML('beforeend',`<g transform="translate(${x},${y})rotate(${r})scale(${s},${s})">${polygonStr}</g>`);
+        g.insertAdjacentHTML('beforeend',`<g x-polygon="${polygon}" transform="translate(${x},${y})rotate(${r})scale(${s},${s})">${polygonStr}</g>`);
     }   
     function clear(args) {
         let stage = args.stage;
+        stageHash = {};
         stage.innerHTML = '';
-    } 
+    }
     function rules(args) {
         let patternShapes = {};
         for (let ri=0;ri<config.rules.length;ri++) {
@@ -91,32 +109,33 @@
                         y = y%config.stage.height; 
                         let r = (shape.r + next.dr)%360;
                         let s = shape.s * next.ds;
+                        let t = 0;
+                        if (!('terminate' in next)) {
+                            t = 0;
+                        } else {
+                            t = next.terminate;
+                        }
                         {
-                            let limit = false;
                             if (s > 1) {
-                                limit = true;
+                                t = 1;
                             }
                             if (s < -1) {
-                                limit = true;
+                                t = 1;
                             }
                             if ('scaleLimit' in config.iteration) {
                                 if (Math.abs(s) < Math.abs(config.iteration.scaleLimit) ) {
-                                    limit = true;
+                                    t = 1;
                                 }
                             }
-                            if (limit){
-                                s = shape.s;
-                                // continue;
-                            }
                         }         
-                        polygonSVG(next.polygon, x, y, r, s, args);
+                        polygonSVG(next.polygon, x, y, r, s, t, args);
                         count++;
                         if ('objectLimit' in config.iteration) {
                             if (count >= config.iteration.objectLimit ) {
                                 console.warn('exceed: config.iteration.objectLimit:'+config.iteration.objectLimit);
                                 return;
                             }
-                        }                       
+                        }                     
                     }
                 }
             }
@@ -133,13 +152,14 @@
                 let svg = document.querySelector('svg');
                 svg.setAttribute('width', config.stage.width);
                 svg.setAttribute('height', config.stage.height);
-                svg.innerHTML = '<g id="stage"></g>';
+                svg.innerHTML = '<g id="terminate"></g><g id="stage"></g>';
                 let count = 0;
                 let args = {
                     dt: config.iteration.dt
                     , stage: document.querySelector('#stage')
+                    , terminate: document.querySelector('#terminate')
                 };
-                polygonSVG(0,0,0,0,1,args);
+                polygonSVG(0,0,0,0,1,0,args);
                 main(count, args);
             }, false);
     }
@@ -173,9 +193,10 @@
     function getPolygons(match,opt) {
         let ret = [];
         let dup = {};
-        let squares = document.querySelectorAll(`polygon${match}`);
+        let squares = document.querySelectorAll(`g#stage polygon${match}`);
         for (let si = 0; si < squares.length; si++) {
             let square = squares[si];
+            let polygon = square.parentNode.getAttribute('x-polygon').trim();
             let transform = square.parentNode.getAttribute('transform').trim();
             let x = 0;
             let y = 0;
@@ -194,17 +215,14 @@
             if (/scale\(([^,\)]+),([^,\)]+)\)/.test(transform)){
                 s = RegExp.$1;
                 s = parseFloat(s);
-            }                
-            let uniq = `${x}:${y}:${r}:${s}`;
-            if (('noDup' in opt) && uniq in dup) {
-                continue;
             }
-            dup[uniq] = true;
+            let p = parseFloat(polygon);
             ret.push({
                 x: x
                 , y: y
                 , r: r
                 , s: s
+                , p: p
             });
         }
         return ret;
