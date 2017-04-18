efd2eaeb1f4928507d3daf678122f339948b6e8a
Yoshiaki Araki <yoshiaki.araki@g-pc-4601.intra.gree-office.net>
2017-04-18 16:21:44 +0900

    simplify without g

--- viscuitta/draw.js
+++ viscuitta/draw.js
@@ -38,7 +38,7 @@
             org = xy.concat([]);
             xy = XY.concat([])
         }
-        polygonList.push(`<g class="touchSensor"><polygon points="${pointList.join(',')}" /></g>`);
+        polygonList.push(`<polygon class="touchSensor"points="${pointList.join(',')}" />`);
     }
     contentList.push(polygonList.join(''));
 
@@ -46,27 +46,84 @@
     document.currentScript.insertAdjacentHTML('afterend',html);
 
     let svg = document.currentScript.nextSibling;
-    let polygons = svg.querySelectorAll('g.touchSensor');
+    let polygons = svg.querySelectorAll('polygon.touchSensor');
     for (let pi=0;pi<polygons.length;pi++) {
-        let center = getCenter(polygons[pi]);
+        let center = calcCenter(polygons[pi]);
+        setCenter(polygons[pi],center);
         polygons[pi].addEventListener('click',function(ev) {
             let target = ev.target;
-            let transform = target.getAttribute('transform');
-            let degree = 0;
-            if (/rotate\s*\(([^\)]+)\)/.test(transform)) {
-                degree = RegExp.$1;
-                degree = parseFloat(degree);
-            }
-            degree+=10;
-            target.setAttribute('transform',`translate(${center.join(',')})rotate(${degree})translate(${negate(center).join(',')})`);
-        });
+            let degree = getRotate(target);
+            degree += 10;
+            setRotate(target,degree);
+        },false);
     }
-    
+    function setCenter (polygon,xy) {
+        let transform = polygon.getAttribute('transform');
+        if (/translate\s*\(([^\)]+)\)rotate\s*\(([^\)]+)\)/.test(transform)) {
+            transform = transform.replace(/translate\s*\([^\)]+\)rotate/,`translate(${xy.join(',')})rotate`);
+            transform = transform.replace(/translate\s*\([^\)]+\)\s*$/,`translate(${negate(xy).join(',')})`);
+        } else {
+            transform = `translate(0,0)translate(${xy.join(',')})rotate(0)scale(1,1)translate(${negate(xy).join(',')})`;
+        }
+        polygon.setAttribute('transform',transform);
+    }
+    function setScale (polygon,xy) {
+        let transform = polygon.getAttribute('transform');
+        if (/scale\s*\([^\)]+\)/.test(transform)) {
+            transform = transform.replace(/scale\s*\([^\)]+\)/,`scale(${xy.join(',')})`);
+        } else {
+            transform = `translate(0,0)translate(0,0)rotate(0)scale(${xy.join(',')})translate(0,0)`;
+        }
+        polygon.setAttribute('transform',transform);
+    }
+    function setRotate (polygon,degree,center) {
+        let transform = polygon.getAttribute('transform');
+        if (/rotate\s*\([^\)]+\)/.test(transform)) {
+            transform = transform.replace(/rotate\s*\([^\)]+\)/,`rotate(${degree})`);
+        } else {
+            transform = `translate(0,0)translate(0,0)rotate(${degree})scale(1,1)translate(0,0)`;
+        }
+        polygon.setAttribute('transform',transform);
+    }
+    function getCenter (polygon) {
+        let transform = polygon.getAttribute('transform');
+        let centerStr = '0,0';
+        if (/translate\s*\(([^\)]+)\)rotate\s*\(([^\)]+)\)/.test(transform)) {
+            centerStr = RegExp.$1;
+        }
+        let center = centerStr = split(',');
+        let xy = [
+            parseFloat(center[0]),
+            parseFloat(center[1])
+        ];
+        return xy;
+    }  
+    function getScale (polygon) {
+        let transform = polygon.getAttribute('transform');
+        let scaleStr = '1,1';
+        if (/scale\s*\(([^\)]+)\)/.test(transform)) {
+            scaleStr = RegExp.$1;
+        }
+        let scale = scaleStr = split(',');
+        let xy = [
+            parseFloat(scale[0]),
+            parseFloat(scale[1])
+        ];
+        return xy;
+    }  
+    function getRotate (polygon) {
+        let transform = polygon.getAttribute('transform');
+        let degree = 0;
+        if (/rotate\s*\(([^\)]+)\)/.test(transform)) {
+            degree = RegExp.$1;
+            degree = parseFloat(degree);
+        }
+        return degree;
+    }
     function negate(xy){
         return [-xy[0],-xy[1]];
     }
-    function getCenter(xpolygon) {
-        let polygon = xpolygon.querySelector('polygon');
+    function calcCenter(polygon) {
         let pointsStr = polygon.getAttribute('points');
         let pointList = pointsStr.split(',');
         let sumx = 0;
