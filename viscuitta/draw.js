f52eb4ec9e5b6698fbbab00fcc03b3e977d2fdd7
Yoshiaki Araki <yoshiaki.araki@g-pc-4601.intra.gree-office.net>
2017-04-19 10:09:09 +0900

    SVG Transform

--- viscuitta/draw.js
+++ viscuitta/draw.js
@@ -1,12 +1,12 @@
 'use strict';
-(function(document,window,console){
+(function (document, window, console) {
     let config = {
         "width": 1200,
         "height": 1200,
         "shapes": [
-            { 
-                angles: [30,150,30,150],
-                lengthes: [100,100,100,100]
+            {
+                angles: [30, 150, 30, 150],
+                lengthes: [100, 100, 100, 100]
             }/*,
              { 
                 angles: [30,150,30,150],
@@ -23,17 +23,17 @@
         ]
     };
     updateConfig();
-    let contentList = []; 
+    let contentList = [];
 
     let polygonList = [];
-    for (let si=0;si<config.shapes.length;si++) {
+    for (let si = 0; si < config.shapes.length; si++) {
         let pointList = [];
-        let org = [1,0];
-        let xy = [0,0];
-        for (var ai=0;ai<config.shapes[si].angles.length;ai++) {
+        let org = [1, 0];
+        let xy = [0, 0];
+        for (var ai = 0; ai < config.shapes[si].angles.length; ai++) {
             var angle = config.shapes[si].angles[ai];
             var length = config.shapes[si].lengthes[ai];
-            let XY = nextPoint(xy,org,angle,length);
+            let XY = nextPoint(xy, org, angle, length);
             pointList.push(XY.join(' '));
             org = xy.concat([]);
             xy = XY.concat([]);
@@ -43,122 +43,112 @@
     contentList.push(polygonList.join(''));
 
     let html = `<svg width="${config.width}" height="${config.height}">${contentList.join('')}</svg>`;
-    document.currentScript.insertAdjacentHTML('afterend',html);
+    document.currentScript.insertAdjacentHTML('afterend', html);
 
     let svg = document.currentScript.nextSibling;
     let polygons = svg.querySelectorAll('polygon.touchSensor');
-    for (let pi=0;pi<polygons.length;pi++) {
+    for (let pi = 0; pi < polygons.length; pi++) {
         let center = calcCenter(polygons[pi]);
-        setCenter(polygons[pi],center);
-        polygons[pi].addEventListener('click',function(ev) {
+        setCenter(polygons[pi], center);
+        polygons[pi].addEventListener('click', function (ev) {
             let target = ev.target;
-            console.log(target.transform);
-            target.transform.baseVal.getItem(0).setScale(1,1);
             let degree = getRotate(target);
             degree += 10;
-            setRotate(target,degree);
-        },false);
+            setRotate(target, degree);
+        }, false);
         // TODO : support drag
     }
     // TODO : more conv functions
-    function setCenter (polygon,xy) {
-        let transform = polygon.getAttribute('transform');
-        if (/translate\s*\(([^\)]+)\)rotate\s*\(([^\)]+)\)/.test(transform)) {
-            transform = transform.replace(/translate\s*\([^\)]+\)rotate/,`translate(${xy.join(',')})rotate`);
-            transform = transform.replace(/translate\s*\([^\)]+\)\s*$/,`translate(${negate(xy).join(',')})`);
-        } else {
-            transform = `translate(0,0)translate(${xy.join(',')})rotate(0)scale(1,1)translate(${negate(xy).join(',')})`;
-        }
-        polygon.setAttribute('transform',transform);
+    function setCenter(polygon, xy) {
+        polygon.transform.baseVal.getItem(1).setTranslate(xy[0],xy[1]);
+        polygon.transform.baseVal.getItem(4).setTranslate(-xy[0],-xy[1]);
     }
-    function setScale (polygon,xy) {
-        let transform = polygon.getAttribute('transform');
-        if (/scale\s*\([^\)]+\)/.test(transform)) {
-            transform = transform.replace(/scale\s*\([^\)]+\)/,`scale(${xy.join(',')})`);
-        } else {
-            transform = `translate(0,0)translate(0,0)rotate(0)scale(${xy.join(',')})translate(0,0)`;
-        }
-        polygon.setAttribute('transform',transform);
+    function setScale(polygon, xy) {
+        polygon.transform.baseVal.getItem(3).setScale(xy[0],xy[1]);
     }
-    function setRotate (polygon,degree,center) {
-        let transform = polygon.getAttribute('transform');
-        if (/rotate\s*\([^\)]+\)/.test(transform)) {
-            transform = transform.replace(/rotate\s*\([^\)]+\)/,`rotate(${degree})`);
-        } else {
-            transform = `translate(0,0)translate(0,0)rotate(${degree})scale(1,1)translate(0,0)`;
-        }
-        polygon.setAttribute('transform',transform);
+    function setRotate(polygon, degree) {
+        polygon.transform.baseVal.getItem(2).setRotate(degree,0,0);
     }
-    function getCenter (polygon) {
-        let transform = polygon.getAttribute('transform');
-        let centerStr = '0,0';
-        if (/translate\s*\(([^\)]+)\)rotate\s*\(([^\)]+)\)/.test(transform)) {
-            centerStr = RegExp.$1;
-        }
-        let center = centerStr = split(',');
-        let xy = [
-            parseFloat(center[0]),
-            parseFloat(center[1])
-        ];
-        return xy;
-    }  
-    function getScale (polygon) {
-        let transform = polygon.getAttribute('transform');
-        let scaleStr = '1,1';
-        if (/scale\s*\(([^\)]+)\)/.test(transform)) {
-            scaleStr = RegExp.$1;
-        }
-        let scale = scaleStr = split(',');
-        let xy = [
-            parseFloat(scale[0]),
-            parseFloat(scale[1])
-        ];
-        return xy;
-    }  
-    function getRotate (polygon) {
-        let transform = polygon.getAttribute('transform');
-        let degree = 0;
-        if (/rotate\s*\(([^\)]+)\)/.test(transform)) {
-            degree = RegExp.$1;
-            degree = parseFloat(degree);
-        }
-        return degree;
+    function getCenter(polygon) {
+        let mat = decomposeMatrix(polygon.getCTM());
+        return mat.center;
     }
-    function negate(xy){
-        return [-xy[0],-xy[1]];
+    function getScale(polygon) {
+        let mat = decomposeMatrix(polygon.getCTM());
+        return mat.scale;
+    }
+    function getRotate(polygon) {
+        let mat = decomposeMatrix(polygon.getCTM());
+        return mat.rotate;
+    }
+
+    function decomposeMatrix(matrix) {
+        // @see https://gist.github.com/2052247
+
+        // calculate delta transform point
+        let px = deltaTransformPoint(matrix, { x: 0, y: 1 });
+        let py = deltaTransformPoint(matrix, { x: 1, y: 0 });
+
+        // calculate skew
+        let skewX = ((180 / Math.PI) * Math.atan2(px.y, px.x) - 90);
+        let skewY = ((180 / Math.PI) * Math.atan2(py.y, py.x));
+
+        return {
+            translate: [
+                matrix.e,
+                matrix.f
+            ],
+            scale: [
+                Math.sqrt(matrix.a * matrix.a + matrix.b * matrix.b),
+                Math.sqrt(matrix.c * matrix.c + matrix.d * matrix.d)
+            ],
+            skew: [skewX, skewY],
+            rotate: skewX,
+            center: [px, py]
+            // rotation is the same as skew x
+        };
+        function deltaTransformPoint(matrix, point) {
+
+            let dx = point.x * matrix.a + point.y * matrix.c + 0;
+            let dy = point.x * matrix.b + point.y * matrix.d + 0;
+            return { x: dx, y: dy };
+        }
+    }
+    function negate(xy) {
+        return [-xy[0], -xy[1]];
     }
     function calcCenter(polygon) {
         let pointsStr = polygon.getAttribute('points');
         let pointList = pointsStr.split(',');
         let sumx = 0;
         let sumy = 0;
-        for (let pi=0;pi<pointList.length;pi++) {
+        for (let pi = 0; pi < pointList.length; pi++) {
             let xy = pointList[pi].split(/\s+/);
             sumx += parseFloat(xy[0]);
             sumy += parseFloat(xy[1]);
         }
-        return [sumx/pointList.length, sumy/pointList.length];
+        return [sumx / pointList.length, sumy / pointList.length];
     }
-    function nextPoint(src,dest,degree,scale) {
-        let radian = (degree/180)*Math.PI;
-        let dir = [dest[0]-src[0], dest[1]-src[1]];
+    function nextPoint(src, dest, degree, scale) {
+        let radian = (degree / 180) * Math.PI;
+        let dir = [dest[0] - src[0], dest[1] - src[1]];
         let cr = Math.cos(radian);
         let sr = Math.sin(radian);
-        let x = dir[0]*cr - dir[1]*sr;
-        let y = dir[0]*sr + dir[1]*cr;
-        let norm = Math.sqrt(x*x+y*y);
+        let x = dir[0] * cr - dir[1] * sr;
+        let y = dir[0] * sr + dir[1] * cr;
+        let norm = Math.sqrt(x * x + y * y);
         return [
-            src[0] + x*scale/norm,
-            src[1] + y*scale/norm 
+            src[0] + x * scale / norm,
+            src[1] + y * scale / norm
         ];
     }
-    function updateConfig () {
+    function updateConfig() {
         let jsonStr = document.currentScript.textContent.trim();
-        if (jsonStr.length>0) {
+        if (jsonStr.length > 0) {
             let tmp = JSON.parse(jsonStr);
             for (var tk in tmp) {
                 config[tk] = tmp[tk];
             }
         }
     }
-})(document,window,console);
+})(document, window, console);
