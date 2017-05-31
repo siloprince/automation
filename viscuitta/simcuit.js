162f0502b8b6a20abf64c3e1e66c6fedd669e80e
Yoshiaki Araki <yoshiaki.araki@g-pc-4601.intra.gree-office.net>
2017-05-31 09:22:39 +0900

    utilities

--- viscuitta/simcuit.js
+++ viscuitta/simcuit.js
@@ -340,92 +340,93 @@ function createObject(svg, objectStr) {
     `);
 }
 // TODO : more conv functions
-function setTranslate(polygon, xy) {
-    polygon.transform.baseVal.getItem(0).setTranslate(xy[0], xy[1]);
-}
-function setCenter(polygon, xy) {
-    polygon.transform.baseVal.getItem(1).setTranslate(xy[0], xy[1]);
-    polygon.transform.baseVal.getItem(4).setTranslate(-xy[0], -xy[1]);
-}
-function setScale(polygon, xy) {
-    polygon.transform.baseVal.getItem(3).setScale(xy[0], xy[1]);
-}
-function setRotate(polygon, degree) {
-    polygon.transform.baseVal.getItem(2).setRotate(degree, 0, 0);
-}
-function getTranslate(polygon) {
-    let mat = decomposeMatrix(polygon.getCTM());
-    return mat.translate;
-}
-function getScale(polygon) {
-    let mat = decomposeMatrix(polygon.getCTM());
-    return mat.scale;
-}
-function getRotate(polygon) {
-    let mat = decomposeMatrix(polygon.getCTM());
-    return mat.rotate;
-}
-function getCenter(polygon) {
-    let mat = decomposeMatrix(polygon.getCTM());
-    /*
-    if (mat.center[0] === 0 && mat.center[1] === 0) {
-        return calcCenter(polygon);
-    }*/
-    return mat.center;
+{
+    function setTranslate(polygon, xy) {
+        polygon.transform.baseVal.getItem(0).setTranslate(xy[0], xy[1]);
+    }
+    function setCenter(polygon, xy) {
+        polygon.transform.baseVal.getItem(1).setTranslate(xy[0], xy[1]);
+        polygon.transform.baseVal.getItem(4).setTranslate(-xy[0], -xy[1]);
+    }
+    function setScale(polygon, xy) {
+        polygon.transform.baseVal.getItem(3).setScale(xy[0], xy[1]);
+    }
+    function setRotate(polygon, degree) {
+        polygon.transform.baseVal.getItem(2).setRotate(degree, 0, 0);
+    }
+    function getTranslate(polygon) {
+        let mat = decomposeMatrix(polygon.getCTM());
+        return mat.translate;
+    }
+    function getScale(polygon) {
+        let mat = decomposeMatrix(polygon.getCTM());
+        return mat.scale;
+    }
+    function getRotate(polygon) {
+        let mat = decomposeMatrix(polygon.getCTM());
+        return mat.rotate;
+    }
+    function getCenter(polygon) {
+        let mat = decomposeMatrix(polygon.getCTM());
+        /*
+        if (mat.center[0] === 0 && mat.center[1] === 0) {
+            return calcCenter(polygon);
+        }*/
+        return mat.center;
 
-    function calcCenter(polygon) {
-        let pointsStr = polygon.getAttribute('points');
-        let pointList = pointsStr.split(',');
-        let sumx = 0;
-        let sumy = 0;
-        for (let pi = 0; pi < pointList.length; pi++) {
-            let xy = pointList[pi].split(/\s+/);
-            sumx += parseFloat(xy[0]);
-            sumy += parseFloat(xy[1]);
+        function calcCenter(polygon) {
+            let pointsStr = polygon.getAttribute('points');
+            let pointList = pointsStr.split(',');
+            let sumx = 0;
+            let sumy = 0;
+            for (let pi = 0; pi < pointList.length; pi++) {
+                let xy = pointList[pi].split(/\s+/);
+                sumx += parseFloat(xy[0]);
+                sumy += parseFloat(xy[1]);
+            }
+            return [sumx / pointList.length, sumy / pointList.length];
         }
-        return [sumx / pointList.length, sumy / pointList.length];
     }
-}
+    function decomposeMatrix(matrix) {
+        // @see https://gist.github.com/2052247
 
-function decomposeMatrix(matrix) {
-    // @see https://gist.github.com/2052247
+        // calculate delta transform point
+        let px = deltaTransformPoint(matrix, { x: 0, y: 1 });
+        let py = deltaTransformPoint(matrix, { x: 1, y: 0 });
 
-    // calculate delta transform point
-    let px = deltaTransformPoint(matrix, { x: 0, y: 1 });
-    let py = deltaTransformPoint(matrix, { x: 1, y: 0 });
+        // calculate skew
+        let skewX = ((180 / Math.PI) * Math.atan2(px.y, px.x) - 90);
+        let skewY = ((180 / Math.PI) * Math.atan2(py.y, py.x));
 
-    // calculate skew
-    let skewX = ((180 / Math.PI) * Math.atan2(px.y, px.x) - 90);
-    let skewY = ((180 / Math.PI) * Math.atan2(py.y, py.x));
+        let cx = 0;
+        let cy = 0;
+        let base = (-matrix.a * matrix.d + matrix.a + matrix.b * matrix.c + matrix.d - 1);
 
-    let cx = 0;
-    let cy = 0;
-    let base = (-matrix.a * matrix.d + matrix.a + matrix.b * matrix.c + matrix.d - 1);
+        cx = ((matrix.d - 1) * matrix.e - matrix.c * matrix.f) / base;
+        cy = ((matrix.a - 1) * matrix.f - matrix.b * matrix.e) / base;
+        if (isNaN(cx)) {
+            cx = 0;
+            cy = 0;
+        }
+        return {
+            translate: [
+                matrix.e,
+                matrix.f
+            ],
+            scale: [
+                Math.sqrt(matrix.a * matrix.a + matrix.b * matrix.b),
+                Math.sqrt(matrix.c * matrix.c + matrix.d * matrix.d)
+            ],
+            skew: [skewX, skewY],
+            rotate: skewX,
+            center: [cx, cy]
+            // rotation is the same as skew x
+        };
+        function deltaTransformPoint(matrix, point) {
 
-    cx = ((matrix.d - 1) * matrix.e - matrix.c * matrix.f) / base;
-    cy = ((matrix.a - 1) * matrix.f - matrix.b * matrix.e) / base;
-    if (isNaN(cx)) {
-        cx = 0;
-        cy = 0;
-    }
-    return {
-        translate: [
-            matrix.e,
-            matrix.f
-        ],
-        scale: [
-            Math.sqrt(matrix.a * matrix.a + matrix.b * matrix.b),
-            Math.sqrt(matrix.c * matrix.c + matrix.d * matrix.d)
-        ],
-        skew: [skewX, skewY],
-        rotate: skewX,
-        center: [cx, cy]
-        // rotation is the same as skew x
-    };
-    function deltaTransformPoint(matrix, point) {
-
-        let dx = point.x * matrix.a + point.y * matrix.c + 0;
-        let dy = point.x * matrix.b + point.y * matrix.d + 0;
-        return { x: dx, y: dy };
+            let dx = point.x * matrix.a + point.y * matrix.c + 0;
+            let dy = point.x * matrix.b + point.y * matrix.d + 0;
+            return { x: dx, y: dy };
+        }
     }
 }
