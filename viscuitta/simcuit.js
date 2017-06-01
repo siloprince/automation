a2d58b0745805df037989fb7c0bea36be3277f60
Yoshiaki Araki <yoshiaki.araki@g-pc-4601.intra.gree-office.net>
2017-06-01 13:21:59 +0900

    todo

--- viscuitta/simcuit.js
+++ viscuitta/simcuit.js
@@ -184,6 +184,11 @@ let ctrlableList = svg.querySelectorAll('.bbox_ctrl_large'); {
                 config.ctrlable.currentCenterY[id] = cty;
                 let cxy = [ctx, cty];
                 setCenter(target, cxy);
+                if(false) {
+                    // TODO
+                    setRotate(target,0);
+                    setTranslate(target,[0,0]);
+                }
                 let centerList = target.querySelectorAll('circle[ctrl="bbox33"]');
                 for (let ci = 0; ci < centerList.length; ci++) {
                     // TODO:
