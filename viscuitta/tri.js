613c3788247af70f44b402a32d30be0da0ea34bc
Yoshiaki Araki <info@tessellation.jp>
2017-10-21 13:02:54 +0900

    ok

--- viscuitta/tri.js
+++ viscuitta/tri.js
@@ -15,6 +15,21 @@
 
     script.insertAdjacentHTML('afterend',`<svg width="1500" height="1500">
     <g transform="translate(700,700)scale(50,50)">
+    <g transform="scale(${s},${s})">
+    <g transform="scale(${s},${s})">
+        <g transform="rotate(30)rotate(30)">
+        <polygon points="${x0},${y0} ${x1},${y1} ${x2},${y2}" opacity="0.2" />
+        </g>
+    </g>
+        <g transform="rotate(30)">
+        <polygon points="${x0},${y0} ${x1},${y1} ${x2},${y2}" opacity="0.4"/>
+        </g>
+    </g>
+    <g transform="scale(1,1)">
+        <polygon points="${x0},${y0} ${x1},${y1} ${x2},${y2}" fill="#ffffff"/>
+    </g>
+
+    <!--
     <g id="ss" transform="scale(${s},${s})rotate(30)">
     <g id="s" transform="scale(${s},${s})rotate(30)">
         <polygon points="${x0},${y0} ${x1},${y1} ${x2},${y2}" opacity="0.2" />
@@ -24,6 +39,7 @@
     <g transform="scale(1,1)">
         <polygon points="${x0},${y0} ${x1},${y1} ${x2},${y2}" fill="#ffffff"/>
     </g>
+    -->
     </g>
   </svg>`);
 })(console,document,window);
