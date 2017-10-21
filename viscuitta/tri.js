9d6b13ce6d251168cfef2a798e7c80a45e4fb248
Yoshiaki Araki <info@tessellation.jp>
2017-10-21 14:52:33 +0900

    regular

--- viscuitta/tri.js
+++ viscuitta/tri.js
@@ -4,7 +4,7 @@
     let script = document.currentScript;
     
     let s = 2.4;
-    let a = -0.5;
+    let a = -0.2;
 
     let x0 = 1;
     let y0 = 0;
@@ -15,6 +15,7 @@
 
     script.insertAdjacentHTML('afterend',`<svg width="1500" height="1500">
     <g transform="translate(700,700)scale(50,50)">
+    <!--
     <g transform="scale(${s},${s})">
     <g transform="scale(${s},${s})">
         <g transform="rotate(30)rotate(30)">
@@ -28,8 +29,8 @@
     <g transform="scale(1,1)">
         <polygon points="${x0},${y0} ${x1},${y1} ${x2},${y2}" fill="#ffffff"/>
     </g>
-
-    <!--
+-->
+    
     <g id="ss" transform="scale(${s},${s})rotate(30)">
     <g id="s" transform="scale(${s},${s})rotate(30)">
         <polygon points="${x0},${y0} ${x1},${y1} ${x2},${y2}" opacity="0.2" />
@@ -38,8 +39,9 @@
     </g>
     <g transform="scale(1,1)">
         <polygon points="${x0},${y0} ${x1},${y1} ${x2},${y2}" fill="#ffffff"/>
+        <circle r="0.1" cx="0" cy="0" fill="#ff0000">
     </g>
-    -->
+    
     </g>
   </svg>`);
 })(console,document,window);
