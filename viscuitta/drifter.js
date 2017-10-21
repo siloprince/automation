4372d5357d3085ab409943e048f08cb4ba87d151
Yoshiaki Araki <info@tessellation.jp>
2017-10-21 19:35:35 +0900

    tmp

--- viscuitta/drifter.js
+++ viscuitta/drifter.js
@@ -12,7 +12,7 @@
     let y2 = Math.sin(Math.PI/6)*Math.sqrt(3)/2;
     let s = 1/Math.sqrt(3);
     script.insertAdjacentHTML('afterend',`<svg width="1500" height="1500">
-    <g transform="translate(700,700)scale(300,300)">
+    <g transform="translate(200,200)scale(300,300)">
     <g transform="scale(1,1)">
         <polygon points="${x0},${y0} ${x1},${y1} ${x2},${y2}" opacity="0.4"/>
         <g transform="rotate(30)scale(${s},${s})">
